a137641d2f6aedb8505ee351c37685f2bef62873
StefanGhiban <stefanghiban962@gmail.com>
2022-04-20 15:44:03 +0300

    lesson

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -22,14 +22,28 @@
 </template>
 
 <script>
+import { useField } from 'vee-validate'
 export default {
   setup () {
     function onSubmit () {
       alert('Submitted')
     }
 
+    const { value: email, errorMessage: errorEmail } = useField('email', (value) => {
+      if (!value) return 'This field is required'
+
+      const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if (!regex.test(String(value).toLowerCase())) {
+        return 'Please enter a valid email address'
+      }
+
+      return true
+    })
+
     return {
-      onSubmit
+      onSubmit,
+      email: email,
+      emailError: errorEmail
     }
   }
 }
